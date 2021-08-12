83968d66dc3aa35ec8dc5188c38ba3d5ee33a453
Jenil Gandhi <87922059+j-brt137@users.noreply.github.com>
2021-08-13 02:34:06 +0530

    1

---
+++ music-list.js
@@ -0,0 +1,55 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Jeff Bezos",
+        artist: "Bo Burnham",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Closer",
+        artist: "Halsey",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "The Bitcoin Anthem",
+        artist: "Team Crypto Point Hindi",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "KAR HAR MAIDAAN FATEH",
+        artist: "Sukhwinder Singh, Shreya Ghoshal",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Vardaan",
+        artist: "CarryMinati x Wily Frenzy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Yun Hi Chala Chal",
+        artist: "Kailash Kher",
+        img: "music-6",
+        src: "music-6"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
